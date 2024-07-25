8248fbc84958ed36e01ced70293a757f3a0d52d4
KagiamamaHIna <kagiyamahino@gmail.com>
2024-07-25 15:47:43 +0800

    小问题

--- files/misc/self/FixedTarget.lua
+++ files/misc/self/FixedTarget.lua
@@ -1,12 +1,13 @@
 dofile_once("mods/wand_editor/files/libs/fn.lua")
 local entity = GetUpdatedEntityID()
-if ModSettingGet(ModID..".locked_target_pos") then
-    local x = GetStorageComp(entity, "fix_pos_x")
-	local y = GetStorageComp(entity, "fix_pos_y")
-    if x == nil or y == nil then
-        x, y = EntityGetTransform(entity)
-        AddSetStorageComp(entity, "fix_pos_x", x)
-        AddSetStorageComp(entity, "fix_pos_y", y)
-    end
-    EntitySetTransform(entity, x, y)
+local x = GetStorageComp(entity, "fix_pos_x")
+local y
+if x == nil then--初始化
+	x, y = EntityGetTransform(entity)
+	AddSetStorageComp(entity, "fix_pos_x", x)
+	AddSetStorageComp(entity, "fix_pos_y", y)
+end
+if ModSettingGet(ModID..".locked_target_pos") then--假设没有模组更改
+    y = GetStorageComp(entity, "fix_pos_y")
+	EntitySetTransform(entity, x, y)
 end
