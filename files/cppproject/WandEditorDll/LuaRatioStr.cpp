79351b0700e9f2b5e17282e8c12da6d50408ca3e
KagiamamaHIna <kagiyamahino@gmail.com>
2024-07-25 08:53:04 +0800

    v1.4.3

--- files/cppproject/WandEditorDll/LuaRatioStr.cpp
+++ files/cppproject/WandEditorDll/LuaRatioStr.cpp
@@ -23,18 +23,25 @@ namespace lua {
 		std::string s2 = luaL_checkstring(L, 2);
 		double score = 0;
 		if (s1.find(s2) != -1) {//如果s2是s1的子串就进行初步匹配，将s2视为主串，检测s1，可知匹配程度
-			score = rapidfuzz::fuzz::partial_ratio(s2, s1);
+			score = rapidfuzz::fuzz::ratio(s1, s2);
 		}
 		std::string temp;
 		std::vector<std::vector<std::string>> pinyinBuf;
+		std::vector<std::vector<std::string>> FirstPinyinBuf;
 		pinyin::Utf8String utf8s1 = pinyin::Utf8String(s1);
 		for (size_t i = 0; i < utf8s1.size(); i++) {//收集数据 
 			if (pinyinData.HasPinyin(utf8s1[i])) {
 				if (!temp.empty()) {//如果不为空,那就保存为一个单一的向量
 					pinyinBuf.push_back(std::vector<std::string>{temp});
+					FirstPinyinBuf.push_back(std::vector<std::string>{temp});
 					temp.clear();
 				}
 				pinyinBuf.push_back(pinyinData.GetPinyin(utf8s1[i]));
+				std::vector<std::string> tempVec;
+				for (auto& v : pinyinData.GetPinyin(utf8s1[i])) {
+					tempVec.push_back(v.substr(0, 1));
+				}
+				FirstPinyinBuf.push_back(tempVec);
 			}
 			else {//如果不是带有拼音的字符就把他们拼接起来处理
 				temp += utf8s1[i];
@@ -50,7 +57,7 @@ namespace lua {
 			if (index == pinyinBuf.size()) {
 				double NewScore = 0;
 				if (current.find(s2) != -1) {//如果s2是排列出来的字符串的子串就进行匹配，操作同上
-					NewScore = rapidfuzz::fuzz::partial_ratio(s2, current);
+					NewScore = rapidfuzz::fuzz::ratio(current, s2);
 				}
 				if (NewScore > score) {
 					score = NewScore;
@@ -59,6 +66,29 @@ namespace lua {
 			}
 			//将下一个索引的所有字符串加入栈中
 			for (const auto& str : pinyinBuf[index]) {
+				stk.push({ index + 1, current + str });
+			}
+		}
+
+		stk = std::stack<std::pair<int, std::string>>();//用栈模拟递归函数行为
+		stk.push({ 0, "" }); //初始索引为0，初始字符串为空
+
+		while (!stk.empty()) {
+			auto [index, current] = stk.top();//结构化绑定赋值
+			stk.pop();
+			//如果索引等于data的大小，表示一个组合已经完成
+			if (index == FirstPinyinBuf.size()) {
+				double NewScore = 0;
+				if (current.find(s2) != -1) {//如果s2是排列出来的字符串的子串就进行匹配，操作同上
+					NewScore = rapidfuzz::fuzz::ratio(current, s2);
+				}
+				if (NewScore > score) {
+					score = NewScore;
+				}
+				continue;
+			}
+			//将下一个索引的所有字符串加入栈中
+			for (const auto& str : FirstPinyinBuf[index]) {
 				stk.push({ index + 1, current + str });
 			}
 		}
